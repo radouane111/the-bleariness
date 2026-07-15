a1e3ecb4e22dd54f4ba72f64977c4112f0992bd8
Radouane El Mhamedi <elmhamediradouane@gmail.com>
2026-07-15 11:33:10 +0200

    Fix TypeScript locale types

--- i18n/request.ts
+++ i18n/request.ts
@@ -3,7 +3,7 @@ import { routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale;
-  if (!locale || !routing.locales.includes(locale as "de" | "en" | "fr" | "ar" | "es")) {
+  if (!locale || !routing.locales.includes(locale as "de" | "en" | "ar")) {
     locale = routing.defaultLocale;
   }
   return {
